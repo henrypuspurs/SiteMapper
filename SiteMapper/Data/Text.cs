576a60310519b10a14195b30f374aa3d9d9c950d
henrypuspurs <henry_puspurs@hotmail.co.uk>
2020-07-24 11:00:04 +0100

    Fixed naming of files

--- SiteMapper/Data/Text.cs
+++ SiteMapper/Data/Text.cs
@@ -9,7 +9,7 @@ namespace SiteMapper.Data
     {
         public void Save(List<string> report)
         {
-            string title = Regex.Match(report[0], @"\.(.*)\.").Groups[1].Value;
+            string title = Regex.Match(report[1], @"\.(.*)\.").Groups[1].Value;
             string content = string.Join("\n", report);
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + $"Report_{title}.txt", content);
         }
